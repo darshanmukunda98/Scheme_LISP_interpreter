25977d85cd001b32c61149cfeab94de5613fc7a0
darshanmukunda98 <darshanmukunda98@github.com>
2022-11-17 14:26:25 +0530

    fixed nested lambdas(closures), 2 params funcs !ok

--- test/prac.js
+++ test/prac.js
@@ -36,7 +36,6 @@ const pop = (input) => {
   if (input.includes(' ') && !input.startsWith('(')) {
     return input.slice(input.indexOf(' '))
   } else {
-    // return input.slice(input.indexOf(')') + 1)
     if (input.endsWith(')') && !input.startsWith('(')) return input.slice(input.indexOf(')'))
   }
 }
@@ -46,7 +45,6 @@ const next = (input) => {
   if (input.includes(' ') && !input.startsWith('(')) {
     return input.substring(0, input.indexOf(' '))
   } else {
-    // return input.substring(0, input.indexOf(')') + 1)
     if (input.endsWith(')') && !input.startsWith('(')) return input.substring(0, input.indexOf(')'))
   }
 }
@@ -58,7 +56,6 @@ function numberEval (atom) { // ignored number validation
 
 // symbolEval
 function symbolEval (atom, GE) { // ignored symbol validation
-  // if (atom === 'x')console.log(GE)
   if (GE[atom] === undefined) { return null }
   return GE[atom]
 }
@@ -66,7 +63,6 @@ function symbolEval (atom, GE) { // ignored symbol validation
 // stringEval
 function stringEval (atom) {
   const matched = atom.match(/".*"/) // ignored string validation  [\w!$%&*/:<=?>~_^+-/*#]+[\w\d]*
-  // if (matched === null) console.log(atom)
   if (!matched[0]) { return null }
 
   return matched[0]
@@ -81,49 +77,29 @@ function atomEval (atom, GE) {
 }
 
 function expressionEval (input, GE) {
-  // console.log(GE)
   if (!input.startsWith('(')) return null
   input = input.slice(1)
 
   const sym = next(input)
   input = pop(input)
-  // if (symbolEval(sym, GE) === null) return null
 
   if (specialForms[sym]) { return specialForms[sym](input, GE) }
 
   input = input.trim()
   const argsArr = getArgs(input).map(arg => evaluator(arg, GE))
-  // const argsArr = getArgs(input, GE)
-  /* while (input) {
-    input = input.trim()
-    if (input[0] === '(') {
-      args.push(atomEval(expressionEval(input.substring(0, nextClosingBracket(input) + 1))))
-      input = input.substring(nextClosingBracket(input) + 1)
-    } else {
-      args.push(atomEval(next(input)))
-      input = pop(input)
-    }
-    input = input.trim()
-    if (!input.includes(' ')) { if (input[0] === ')') break }
-  } */
+
   if (!input.endsWith(')')) return null
   input = input.slice(1)
   if (sym[0] === '(') {
-    // console.log(input)
-    // const op = expressionEval(sym, GE)
     const op = evaluator(sym, GE)
     return op(...argsArr)
   }
   if (atomEval(sym, GE) === null) return null
-  // console.log(argsArr)
-  // if (typeof GE[sym] !== 'function') { console.log((GE[sym].toString() + ' ' + argsArr + ' ' + sym)) }
 
-  // if (typeof GE[sym] === 'function') return GE[sym](argsArr)
+  if (typeof GE[sym] === 'object') GE[sym] = GE[sym][0]
   if (GE[sym] !== 'undefined') return GE[sym](argsArr)
-  /* return args.reduce(GE[sym]) */
 }
 function evaluator (expression, GE) {
-  // console.log(GE)
   if (expression[0] === '(') { return expressionEval(expression, GE) }
   return atomEval(expression, GE)
 }
@@ -133,10 +109,10 @@ function getArgs (input) {
   while (input) {
     input = input.trim()
     if (input[0] === '(') {
-      argsArr.push(/* atomEval( evaluator( */input.substring(0, nextClosingBracket(input) + 1)/* , GE */)/* ) ) */
+      argsArr.push(input.substring(0, nextClosingBracket(input) + 1))
       input = input.substring(nextClosingBracket(input) + 1)
     } else {
-      argsArr.push(/* atomEval( */next(input)/* , GE) */)
+      argsArr.push(next(input))
       input = pop(input)
     }
     input = input.trim()
@@ -153,9 +129,6 @@ function ifParser (input, GE) {
   input = pop(input)
   const falseExp = next(input)
   input = pop(input)
-  /* if (condition) {
-    return trueExp
-  } else { return falseExp } */
   return condition ? evaluator(trueExp, GE) : evaluator(falseExp, GE)
 }
 function defineParser (input, GE) {
@@ -169,28 +142,22 @@ function defineParser (input, GE) {
 
 function lambdaParser (input, GE) {
   input = input.trim()
-  // console.log(input)
+
   if (input[0] !== '(') { return null }
   let args = next(input)
 
   args = args.substring(1, args.length - 1)
   if (args.includes(' ')) { args = args.split(' ') }
 
-  console.log(args)
   input = pop(input)
   const exp = next(input)
-  // console.log(exp)
+
   input = pop(input)
 
-  // const localEnv = GE
-  // let localEnv
-
   function lambdaFunc (...funcArgs) {
-    // localEnv = GE
     const localEnv = Object.create(GE)
-    funcArgs.forEach((arg, index) => { localEnv[args] = arg/* [index] */ /* console.log(eval('localEnv[args] = arg[index]')) */ })
-    console.log(localEnv)
-    console.log(funcArgs + ' ' + exp)
+    funcArgs.forEach((arg, index) => { localEnv[args] = arg })
+
     return evaluator(exp, localEnv)
   }
   return lambdaFunc
@@ -206,79 +173,84 @@ function setParser (input, GE) {
   input = pop(input)
   if (GE[symbol] === undefined) { console.log('can\'t set undefined variable') }
   GE[symbol] = evaluator(expression, GE)
-  // console.log(GE)
+
   return `${symbol} Set`
 }
 function beginParser (input, GE) {
   const argsArr = getArgs(input).map(arg => evaluator(arg, GE))
 
-  // argsArr.slice(0, argsArr.length - 1).forEach(arg => expressionEval(arg))
-  return /* expressionEval( */argsArr[argsArr.length - 1]/* ) */
+  return argsArr[argsArr.length - 1]
 }
 function main (input) {
   return evaluator(input, GE)
 }
 
 /* const input = process.argv[2]
-console.log(evaluator(input, GE))
+console.log(main(input))
 // console.log(atomEval(input)) */
-/*
-// ______________________________Math Cases_______________________________
+
+/* // ______________________________Math Cases_______________________________
 console.log('Math')
-console.log(evaluator('-5 ', GE) === -5)
-console.log(evaluator('pi', GE) === 3.141592653589793)
-console.log(evaluator('-5', GE) === -5)
-console.log(evaluator('(sqrt (/ 8 2))', GE) === 2)
-console.log(evaluator('(* (/ 1 2) 3)', GE) === 1.5)
-console.log(evaluator('(+ 1 (+ 2 3))', GE) === 6)
-console.log(evaluator('( + ( + ( + 9 (+ 2 2)) 2) ( + 3 4) )', GE) === 22)
-console.log(evaluator('(+ (+ 1 (- 1 1)) 1)', GE) === 2)
-console.log(evaluator('(* 5 10)', GE) === 50)
-console.log(evaluator('( * 1 2 ( / 10 4 2 ) ( + 6 5 ( - 1 2 3 ) ) ( + 2 3 ) )', GE) === 87.5)
+console.log(main('-5 ') === -5)
+console.log(main('pi' ) === 3.141592653589793)
+console.log(main('-5' ) === -5)
+console.log(main('(sqrt (/ 8 2))' ) === 2)
+console.log(main('(* (/ 1 2) 3)' ) === 1.5)
+console.log(main('(+ 1 (+ 2 3))' ) === 6)
+console.log(main('( + ( + ( + 9 (+ 2 2)) 2) ( + 3 4) )' ) === 22)
+console.log(main('(+ (+ 1 (- 1 1)) 1)' ) === 2)
+console.log(main('(* 5 10)' ) === 50)
+console.log(main('( * 1 2 ( / 10 4 2 ) ( + 6 5 ( - 1 2 3 ) ) ( + 2 3 ) )' ) === 87.5)
 
 // _____________________________________if_______________________________
 console.log('If')
-console.log(evaluator('( if (> 30 45) (+ 1 1) "failedOutput")', GE) === '"failedOutput"')
-console.log(evaluator('(if (= 12 12) (+ 78 2) 9)', GE) === 80)
-console.log(evaluator('(if #f 1 0)', GE) === 0)
-console.log(evaluator('(if #t "abc" 1)', GE) === '"abc"')
+console.log(main('( if (> 30 45) (+ 1 1) "failedOutput")' ) === '"failedOutput"')
+console.log(main('(if (= 12 12) (+ 78 2) 9)' ) === 80)
+console.log(main('(if #f 1 0)' ) === 0)
+console.log(main('(if #t "abc" 1)' ) === '"abc"')
 
 // ____________________________define____________________________________
 console.log('Define')
-console.log(evaluator('(define a 90)', GE))
-console.log(evaluator('a', GE))
-console.log(evaluator('(set! a 100)', GE))
-console.log(evaluator('a', GE))
+console.log(main('(define a 90)' ))
+console.log(main('a' ))
+console.log(main('(set! a 100)' ))
+console.log(main('a' ))
 console.log(GE.a)
-console.log(evaluator('(define x (+ 5 5))', GE))
+console.log(main('(define x (+ 5 5))' ))
 
 // _____________________________________quote____________________________________
 console.log('Quote')
-console.log(evaluator('(quote (a b c))', GE))
-console.log(evaluator('(quote (+ 1 2)) ', GE))
+console.log(main('(quote (a b c))' ))
+console.log(main('(quote (+ 1 2)) ' ))
 
 // _________________________begin__________________________________________
 
 console.log('Begin')
-console.log(evaluator('(begin (define r 10) (* pi (* r r)))', GE) === 314.1592653589793)
-console.log(evaluator('(begin (define r 10) (* pi (* r r)) (define a 100) (+ a a) 5)', GE) === 5) */
+console.log(main('(begin (define r 10) (* pi (* r r)))' ) === 314.1592653589793)
+console.log(main('(begin (define r 10) (* pi (* r r)) (define a 100) (+ a a) 5)' ) === 5) */
 
 // ____________________________lambda and higher order functions_____________
 console.log('Lambda')
-console.log(evaluator('((lambda (x) (+ x x)) (* 3 4))', GE) === 24)
-console.log(typeof (evaluator('(lambda (x) (+ x x))', GE)) === 'function')
+console.log(main('((lambda (x) (+ x x)) (* 3 4))') === 24)
+console.log(typeof (main('(lambda (x) (+ x x))')) === 'function')
 
-evaluator('(define x 4)', GE)
-console.log(evaluator('((lambda (y) (+ y x)) 5)', GE) === 9)
+main('(define x 4)')
+console.log(main('((lambda (y) (+ y x)) 5)') === 9)
 
-evaluator('(define twice (lambda (x) (* 2 x)))', GE)
+main('(define twice (lambda (x) (* 2 x)))')
 
-console.log(evaluator('(twice 5)', GE) === 10)
+console.log(main('(twice 5)') === 10)
 
-evaluator('(define repeat (lambda (f) (lambda (x) (f (f x)))))', GE)
+main('(define repeat (lambda (f) (lambda (x) (f (f x)))))')
 
-console.log(evaluator('((repeat twice) 10)', GE))// === 40)
-/* console.log(evaluator('((lambda (x) x) 10)', GE))
-console.log(evaluator('(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))', GE))
-console.log(evaluator('(fact 10)', GE))
-console.log(evaluator('((repeat twice) 10)', GE)) //= == 40) */
+console.log(main('((repeat twice) 10)') === 40)
+console.log(main('((repeat (repeat (repeat (repeat twice)))) 10)'))
+console.log(main('((lambda (x) x) 10)'))
+console.log(main('(define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1))))))'))
+console.log(main('(fact 10)'))
+
+// ____________________________nested lambda(closures)______________________________________
+console.log('Nested lambda')
+main('(define rectangleArea (lambda (length) (lambda (bredth) (* length bredth))))')
+main('(define areaLen2 (rectangleArea 2))')
+console.log(main('(areaLen2 3)') === 6)
